7c3ae188ff16390a0a331e10554198cc2f515547
Peggy Griffin <seishingithub@gmail.com>
2014-06-22 18:44:27 -0600

    User can create and view list of people

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,29 @@
+class PeopleController < ApplicationController
+
+  def index
+    @people = Person.all
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_parameters)
+    if @person.save
+      redirect_to people_path
+    else
+      render :new
+    end
+  end
+
+  #def show
+  #  @person = Person.find(params[:id])
+  #end
+
+  private
+  def person_parameters
+    params.require(:person).permit(:name, :description)
+  end
+
+end
