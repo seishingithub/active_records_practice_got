be59e8bc7354821b5135834968c7613f63238964
Peggy Griffin <seishingithub@gmail.com>
2014-06-22 20:09:00 -0600

    User can view person's 'show page'

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,9 +17,9 @@ class PeopleController < ApplicationController
     end
   end
 
-  #def show
-  #  @person = Person.find(params[:id])
-  #end
+  def show
+    @person = Person.find(params[:id])
+  end
 
   private
   def person_parameters
--- spec/features/person_spec.rb
+++ spec/features/person_spec.rb
@@ -10,6 +10,17 @@ feature 'Manage People' do
     click_on 'Create Person'
     expect(page).to have_content 'Arya'
     expect(page).to have_content 'feisty'
-
+  end
+  scenario 'User can edit and update list of people' do
+    visit '/'
+    click_on 'Add a Person'
+    fill_in 'Name', with: 'Arya'
+    fill_in 'Description', with: 'feisty'
+    click_on 'Create Person'
+    expect(page).to have_content 'Arya'
+    expect(page).to have_content 'feisty'
+    click_on 'Show'
+    expect(page).to have_content 'Arya'
+    expect(page).to have_content 'feisty'
   end
 end
